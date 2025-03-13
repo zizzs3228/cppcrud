bacbac4ed2fc4ba1bfb7c1bb211d7c9b7ed6f59f
zizzs zizzsovskiy <zizzszizzsovskiy@MacBook-Pro-zizzs.local>
2025-03-13 15:03:10 +0300

    readme and new features

---
+++ README.md
@@ -0,0 +1,8 @@
+## Basic commands
+
+1) add - add task
+2) show - show all task in creation order
+3) show -p - show all tasks in priority order
+4) update - update task
+5) delete - delete task
+6) search <query> - search in name and description of the task
--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@
 #include <map>
 #include <nlohmann/json.hpp>
 #include <fstream>
+#include <vector>
+#include <utility>
+#include <chrono>
+#include <format>
 
 class CRUD {
 private:
     std::map<int, std::map<std::string, std::string>> tasks;
+
+    std::string get_current_iso8601_time() {
+        std::time_t now = std::time(nullptr);
+        std::tm *gmt = std::gmtime(&now);
+        char buffer[32];
+        std::strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H:%M:%SZ", gmt);
+        return std::string(buffer);
+    }
 
     void load_tasks_json() {
         std::ifstream infile("tasks.json");
@@ -22,6 +34,7 @@ private:
             std::map<std::string, std::string> task_details = it.value().get<std::map<std::string, std::string>>();
             tasks[task_id] = task_details;
         }
+        infile.close();
         std::cout << "Tasks loaded successfully from tasks.json" << std::endl;
     }
 
@@ -36,6 +49,7 @@ private:
             return;
         }
         outfile << j.dump(4);
+        outfile.close();
     }
 
     void show_tasks() {
@@ -46,27 +60,96 @@ private:
         for (const auto& task : tasks) {
             std::cout << "Task: №" << task.first << std::endl;
             std::cout << "Task name: " << task.second.at("name") << std::endl;
-            std::cout << "Description: " << task.second.at("description") << std::endl << std::endl;
+            std::cout << "Description: " << task.second.at("description") << std::endl;
+            std::cout << "Priority: " << task.second.at("priority") << std::endl;
+            std::cout << "Created at: " << task.second.at("created_at") << std::endl << std::endl;
+        }
+    }
+
+    void show_tasks_by_priority(){
+        if (tasks.empty()) {
+            std::cout << "No tasks" << std::endl;
+            return;
+        }
+        std::vector<std::pair<int, std::map<std::string, std::string>>> sorted_tasks(tasks.begin(), tasks.end());
+        std::sort(sorted_tasks.begin(), sorted_tasks.end(), [](const std::pair<int, std::map<std::string, std::string>>& a, const std::pair<int, std::map<std::string, std::string>>& b){
+            return std::stoi(a.second.at("priority")) > std::stoi(b.second.at("priority"));
+        });
+        for (const auto& task:sorted_tasks){
+            std::cout << "Task: №" << task.first << std::endl;
+            std::cout << "Task name: " << task.second.at("name") << std::endl;
+            std::cout << "Description: " << task.second.at("description") << std::endl;
+            std::cout << "Priority: " << task.second.at("priority") << std::endl;
+            std::cout << "Created at: " << task.second.at("created_at") << std::endl << std::endl;
+        }
+    }
+
+    void find_in_text(const std::string& search_query){
+        if (tasks.empty()) {
+            std::cout << "No tasks" << std::endl;
+            return;
+        }
+        bool found = false;
+        for (const auto& task:tasks){
+            if (task.second.at("name").find(search_query) != std::string::npos || task.second.at("description").find(search_query) != std::string::npos){
+                std::cout << "Task: №" << task.first << std::endl;
+                std::cout << "Task name: " << task.second.at("name") << std::endl;
+                std::cout << "Description: " << task.second.at("description") << std::endl;
+                std::cout << "Priority: " << task.second.at("priority") << std::endl;
+                std::cout << "Created at: " << task.second.at("created_at") << std::endl << std::endl;
+                found = true;
+            }
+        }
+        if (!found){
+            std::cout << "No tasks found with the search query" << std::endl;
+        }
+    }
+
+    std::string read_valid_priority() {
+        while (true) {
+            std::cout << "Enter priority (from 0 to 1000, 1000 is most important): ";
+            std::string input;
+            std::getline(std::cin, input);
+            try {
+                // Попытка преобразования строки в число
+                int priority = std::stoi(input);
+                // Проверка диапазона
+                if (priority < 0 || priority > 1000) {
+                    std::cout << "Priority must be between 0 and 1000. Please try again." << std::endl;
+                    continue;
+                }
+                // Можно дополнительно проверить длину строки, если нужно (например, не более 4 символов)
+                if (input.size() > 4) {
+                    std::cout << "Input too long. Please enter a valid number." << std::endl;
+                    continue;
+                }
+                return input;
+            } catch (std::exception& e) {
+                std::cout << "Invalid input. Please enter a number between 0 and 1000." << std::endl;
+            }
         }
     }
 
     void handle_add_task() {
+        std::cout << "Enter task name: ";
+        std::string task_name;
+        std::getline(std::cin, task_name);
+        std::cout << "Enter description: ";
+        std::string description;
+        std::getline(std::cin, description);
+        std::string priority = read_valid_priority();
         if (tasks.empty()) {
-            std::cout << "Enter task name: ";
-            std::string task_name;
-            std::getline(std::cin, task_name);
-            std::cout << "Enter description: ";
-            std::string description;
-            std::getline(std::cin, description);
-            tasks[1] = {{"name", task_name}, {"description", description}};
+            tasks[1] = {{"name", task_name}, 
+                {"description", description}, 
+                {"priority", priority},
+                {"created_at", get_current_iso8601_time()}
+            };
         } else {
-            std::cout << "Enter task name: ";
-            std::string task_name;
-            std::getline(std::cin, task_name);
-            std::cout << "Enter description: ";
-            std::string description;
-            std::getline(std::cin, description);
-            tasks[tasks.rbegin()->first + 1] = {{"name", task_name}, {"description", description}};
+            tasks[tasks.rbegin()->first + 1] = {{"name", task_name}, 
+                {"description", description}, 
+                {"priority", priority},
+                {"created_at", get_current_iso8601_time()}
+            };
         }
         save_tasks_json();
     }
@@ -90,6 +173,7 @@ private:
         std::cout << "What do you want to update?" << std::endl;
         std::cout << "1. Task name" << std::endl;
         std::cout << "2. Description" << std::endl;
+        std::cout << "3. Priority" << std::endl;
         int choice;
         std::cin >> choice;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -103,6 +187,10 @@ private:
             std::string description;
             std::getline(std::cin, description);
             tasks[task_number]["description"] = description;
+        } else if (choice == 3) {
+            std::string priority = read_valid_priority();
+            tasks[task_number]["priority"] = priority;
+
         } else {
             std::cout << "Invalid choice" << std::endl;
             handle_update_task();
@@ -131,7 +219,12 @@ public:
             }
             if (command == "show") {
                 show_tasks();
-            } else if (command == "add") {
+            } else if (command == "show -p") {
+                show_tasks_by_priority();
+            } else if (command.find("search") == 0) {
+                find_in_text(command.substr(7));
+            }
+            else if (command == "add") {
                 handle_add_task();
             } else if (command == "update") {
                 handle_update_task();
